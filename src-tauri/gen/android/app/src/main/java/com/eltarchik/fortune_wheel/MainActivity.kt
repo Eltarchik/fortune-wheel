4bd60890b1d376fe6cbcb9ecc0117a288bbaadae
unknown <makarteni@gmail.com>
2025-10-14 21:51:18 +0300

    init

---
+++ src-tauri/gen/android/app/src/main/java/com/eltarchik/fortune_wheel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eltarchik.fortune_wheel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
